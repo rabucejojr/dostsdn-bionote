616dc6b32f3bfa54ed42f0badc3440dff684cf23
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-12-18 17:03:46 +0800

    Add placeholder

--- src/components/ViewCard.vue
+++ src/components/ViewCard.vue
@@ -5,12 +5,12 @@
   <div>
     <div class="flex flex-col items-center justify-center min-h-screen">
       <!-- Background Image -->
-      <div class="w-2/5 h-64 bg-gray-400 rounded-2xl flex items-center justify-center mt-[-24px] overflow-hidden">
+      <div class="w-2/5 h-64 bg-white rounded-2xl flex items-center justify-center mt-[-24px] overflow-hidden">
         <img src="/src/assets/images/bg-2.png" alt="Profile Image" class="border-white">
       </div>
 
       <!-- Profile Section -->
-      <div class="w-1/3 h-96 bg-black rounded-2xl flex items-center justify-center relative top-[-60px]">
+      <div class="w-1/3 h-96 rounded-2xl flex items-center justify-center relative top-[-60px]">
         <div>
           <div class="absolute top-[-60px] left-1/2 transform -translate-x-1/2">
             <div class="w-32 h-32 rounded-full bg-gray-200 overflow-hidden flex items-center justify-center">
@@ -19,10 +19,17 @@
           </div>
           <!-- Personal Details -->
           <div class="">
-            <div class="text-center text-white">
+            <div class="text-center">
               <h2 class="text-xl font-bold">Mariel P. Makinano</h2>
               <p class="text-sm">Provincial Director, PSTO-SDN</p>
               <p class="text-sm">Surigao City, Surigao del Norte</p>
+              <p class="text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores veniam rem autem vitae hic fuga quas numquam officia eligendi.</p>
+              <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus natus hic cum possimus illo consequuntur sint dicta amet voluptatum dolores officia recusandae, autem suscipit officiis! Soluta voluptatum delectus, repellendus mollitia eveniet perspiciatis non enim iure voluptates velit dignissimos harum ad?</p>
+            </div>
+            <div class="text-center">
+              <div class=" text-sm">
+                Logo
+              </div>
             </div>
           </div>
         </div>
