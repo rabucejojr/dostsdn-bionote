90699b84a178836840af38935c5387f125f3360f
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-12-18 20:27:35 +0800

    add global google font

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,14 @@ import ViewCard from './components/ViewCard.vue';
 <template>
   <ViewCard/>
 </template>
+
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
+*{
+  font-family: "Roboto Slab", serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
