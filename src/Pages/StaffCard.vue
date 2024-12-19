408c872a0550dc561df9f3ae6bde858636213df6
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-12-19 11:51:37 +0800

    Add grid-cols-5 layout

--- src/Pages/StaffCard.vue
+++ src/Pages/StaffCard.vue
@@ -6,8 +6,8 @@ import ProfileImage from '@/components/ProfileImage.vue';
 
 <template>
     <div class="flex items-center justify-center min-h-screen bg-gray-50">
-      <div class="text-center">
-        <!-- Top Level (Manager or CEO) -->
+      <div class="text-center grid grid-cols-5">
+
         <div class="flex justify-center space-x-6 mb-4">
           <div class="text-center">
             <ProfileImage profile="/images/mpm.jpg" />
@@ -17,8 +17,7 @@ import ProfileImage from '@/components/ProfileImage.vue';
           </div>
           </div>
         </div>
-        <!-- Second Level (Leads/Heads) -->
-        <div class="flex justify-center space-x-6 mb-4">
+
           <div class="text-center justify-center mb-4 sm:mb-2">
             <ProfileImage profile="/images/mgms.jpg" />
             <div class="flex flex-col items-center justify-center">
@@ -32,10 +31,8 @@ import ProfileImage from '@/components/ProfileImage.vue';
               <p class="text-xs mt-1">Mildred Eileen Ronsable</p>
               <p class="text-xs">SRS II</p>
             </div>
-          </div>
         </div>
-        <!-- Third Level (Team Members) -->
-        <div class="flex flex-wrap justify-center space-x-6 mb-4">
+
           <div class="text-center mb-4 sm:mb-2">
             <ProfileImage profile="/images/rjit.jpg" />
             <div class="flex flex-col items-center justify-center">
@@ -57,9 +54,6 @@ import ProfileImage from '@/components/ProfileImage.vue';
               <p class="text-xs">PTA I</p>
             </div>
           </div>
-        </div>
-        <!-- Fourth Level (Staff Members) -->
-        <div class="flex flex-wrap justify-center space-x-6">
           <div class="text-center mb-4 sm:mb-2">
             <ProfileImage profile="/images/jab.jpg" />
             <div class="flex flex-col items-center justify-center">
@@ -74,22 +68,21 @@ import ProfileImage from '@/components/ProfileImage.vue';
               <p class="text-xs">PTA I</p>
             </div>
           </div>
-          <div class="text-center mb-4 sm:mb-2">
-            <ProfileImage profile="/images/joa.jpg" />
-            <div class="flex flex-col items-center justify-center">
-              <p class="text-xs mt-1">Jovan Agbu</p>
-              <p class="text-xs">PTA II, B2MEIC</p>
+            <div class="text-center mb-4 sm:mb-2">
+              <ProfileImage profile="/images/joa.jpg" />
+              <div class="flex flex-col items-center justify-center">
+                <p class="text-xs mt-1">Jovan Agbu</p>
+                <p class="text-xs">PTA II, B2MEIC</p>
+              </div>
             </div>
-          </div>
-          <div class="text-center mb-4 sm:mb-2">
-            <ProfileImage profile="/images/rha.jpg" />
-            <div class="flex flex-col items-center justify-center">
-              <p class="text-xs mt-1">Roger Abucejo, Jr.</p>
-              <p class="text-xs">Support Staff</p>
+            <div class="text-center mb-4 sm:mb-2">
+              <ProfileImage profile="/images/rha.jpg" />
+              <div class="flex flex-col items-center justify-center">
+                <p class="text-xs mt-1">Roger Abucejo, Jr.</p>
+                <p class="text-xs">Support Staff</p>
+              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
 </template>
 
