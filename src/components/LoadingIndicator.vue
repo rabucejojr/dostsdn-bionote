4ba3bca6d853f51e654a05fb65d39157f8be9fe6
Jeffrey Bernadas <jeffrey.bernadas0@gmail.com>
2024-12-23 20:32:32 +0800

    Feat: Add size prop in Loader component

--- src/components/LoadingIndicator.vue
+++ src/components/LoadingIndicator.vue
@@ -1,5 +1,24 @@
+<script setup>
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'sm',
+  }
+});
+
+const sizeClasses = {
+  sm: 'w-6 h-6',
+  md: 'w-8 h-8',
+  lg: 'w-12 h-12'
+};
+
+const currentSize = computed(() => sizeClasses[props.size]);
+</script>
+
 <template>
-  <div class="loader"></div>
+  <div :class="['loader', currentSize]"></div>
 </template>
 
 <style scoped>
@@ -7,8 +26,6 @@
   border: 4px solid rgba(0, 0, 0, 0.1);
   border-top: 4px solid #3498db;
   border-radius: 50%;
-  width: 24px;
-  height: 24px;
   animation: spin 1s linear infinite;
 }
 
@@ -16,6 +33,7 @@
   0% {
     transform: rotate(0deg);
   }
+
   100% {
     transform: rotate(360deg);
   }
