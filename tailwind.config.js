a2431d3e5bf53d05328c57c4bd53d4d8e1535911
Jeffrey Bernadas <jeffrey.bernadas0@gmail.com>
2024-12-23 20:31:58 +0800

    Feat: Add additional tailwind configs and scrollbar styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        18: '4.5rem',
+      },
+      screens: {
+        '2xs': '300px',
+        xs: '345px',
+      },
+    },
   },
   plugins: [],
 }
