89f21c5d157892c6785b771de9045cc04161aeaa
Roger Abucejo Jr <rogerhapay@gmail.com>
2024-12-23 17:39:01 +0800

    Add Scoped Styles

--- src/Pages/MainDashboard.vue
+++ src/Pages/MainDashboard.vue
@@ -1,7 +1,7 @@
 <script setup>
-import PageHeader from '@/components/PageHeader.vue'
-import AllStaff from './AllStaff.vue'
-import PageFooter from '@/components/PageFooter.vue'
+import PageHeader from "@/components/PageHeader.vue";
+import AllStaff from "./AllStaff.vue";
+import PageFooter from "@/components/PageFooter.vue";
 </script>
 
 <template>
--- src/components/StaffCard.vue
+++ src/components/StaffCard.vue
@@ -1,9 +1,12 @@
-<script setup>import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import axios from 'axios'
-import ProfilePicture from '@/components/ProfilePicture.vue'
-import LoadingIndicator from '@/components/LoadingIndicator.vue'
-import { RiArrowLeftLine } from '@remixicon/vue'
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import axios from "axios";
+import ProfilePicture from "@/components/ProfilePicture.vue";
+import LoadingIndicator from "@/components/LoadingIndicator.vue";
+import { RiArrowLeftLine } from "@remixicon/vue";
+import PageHeader from "./PageHeader.vue";
+import PageFooter from "./PageFooter.vue";
 
 const route = useRoute()
 const personnel = ref(null)
@@ -11,14 +14,14 @@ const isLoading = ref(true)
 
 const fetchPersonnelData = async (personnelId) => {
   try {
-    isLoading.value = true
-    const response = await axios.get('/infos.json')
-    const staffData = response.data
-    personnel.value = staffData.find((staff) => staff.id === personnelId)
-    isLoading.value = false
+    isLoading.value = true;
+    const response = await axios.get("/infos.json");
+    const staffData = response.data;
+    personnel.value = staffData.find((staff) => staff.id === personnelId);
+    isLoading.value = false;
   } catch (error) {
-    isLoading.value = false
-    console.error('Error fetching staff data:', error)
+    isLoading.value = false;
+    console.error("Error fetching staff data:", error);
   }
 }
 
@@ -29,27 +32,35 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="min-h-screen">
-    <div class="min-h-screen flex items-center justify-center flex-col">
+  <div class="fixed top-0 left-0 w-full z-50 shadow-sm sm:px-4 sm:py-2 md:px-6 md:py-3">
+    <PageHeader />
+  </div>
+  <div class="flex flex-col pt-32">
+    <div class="absolute top-28 md:top-70 left-4 items-start">
+      <router-link to="/" class="underline inline-block">
+        <button><RiArrowLeftLine /></button>
+      </router-link>
+    </div>
+    <div class="min-h-screen flex items-center justify-center">
       <div
-        class="min-h-[375px] w-full max-w-[90%] sm:max-w-md md:max-w-lg bg-gray-200 rounded-2xl shadow-2xl p-4 sm:p-8 relative"
+        class="w-full max-w-md md:max-w-lg bg-gray-200 rounded-2xl shadow-2xl p-8 relative"
       >
         <!-- Background Shapes -->
         <div
           class="absolute top-4 left-4 w-16 h-16 bg-blue-700 opacity-20 rounded-full md:w-20 md:h-20 lg:w-24 lg:h-24"
-        />
+        ></div>
         <div
           class="absolute bottom-5 right-5 w-24 h-24 bg-indigo-700 opacity-20 rounded-full md:w-32 md:h-32 lg:w-36 lg:h-36"
-        />
+        ></div>
         <div
           class="absolute top-1/4 left-1/4 w-12 h-12 bg-green-500 opacity-25 rounded-full md:w-16 md:h-16 lg:w-20 lg:h-20"
-        />
+        ></div>
         <div
           class="absolute bottom-1/4 right-1/3 w-20 h-20 bg-pink-500 opacity-30 rounded-full md:w-24 md:h-24 lg:w-28 lg:h-28"
-        />
+        ></div>
         <div
           class="absolute top-8 right-8 w-20 h-20 bg-yellow-500 opacity-20 rounded-full md:w-28 md:h-28 lg:w-32 lg:h-32"
-        />
+        ></div>
         <!-- Content -->
         <div v-if="isLoading" class="flex justify-center items-center min-h-[300px]">
           <LoadingIndicator size="lg" />
@@ -59,7 +70,9 @@ onMounted(() => {
             :profile="personnel.profile"
             class="mb-2 mx-auto rounded-full justify-items-center"
           />
-          <h2 class="text-center text-xl font-bold text-gray-800">{{ personnel.name }}</h2>
+          <h2 class="text-center text-xl font-bold text-gray-800">
+            {{ personnel.name }}
+          </h2>
           <p class="text-center text-gray-600">{{ personnel.designation }}</p>
           <p class="text-center text-gray-600">
             <a :href="`mailto:${personnel.email}`">{{ personnel.email }}</a>
