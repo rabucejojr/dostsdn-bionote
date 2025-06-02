f211de39ac022a7170f66f4c773f67244bbb286e
Roger Abucejo Jr <rogerhapay@gmail.com>
2025-06-02 08:53:33 +0800

    add componentheader to maindashboard

--- src/Pages/MainDashboard.vue
+++ src/Pages/MainDashboard.vue
@@ -1,12 +1,15 @@
 <script setup>
 import PageHeader from '@/components/PageHeader.vue'
 import PageFooter from '@/components/PageFooter.vue'
+import ContactHeader from '@/components/ContactHeader.vue'
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen h-screen">
     <!-- Header -->
     <PageHeader />
+    <!-- Contact Header -->
+    <ContactHeader />
     <!-- Main Content -->
     <router-view />
     <!-- Footer -->
